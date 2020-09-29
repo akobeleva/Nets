130c3ad65c808bfd38dc10c2b27171aec4ef8ebb
Anastasia Kobeleva <a.kobeleva@g.nsu.ru>
2020-09-29 22:50:48 +0700

    fix recv

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 
 public class App {
     public static void main(String[] args) throws IOException {
-        Client client = new Client(args[0]);
+        Client client = new Client(args[0], Integer.parseInt(args[1]));
         client.findCopies();
     }
 }
--- src/Client.java
+++ src/Client.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.net.*;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
 import java.util.UUID;
 
@@ -9,46 +10,49 @@ public class Client {
     private MulticastSocket socket;
     private final long timeout = 5000;
     private Map<UUID, Long> aliveCopies;
-    byte[] sendData = new byte[512];
-    byte[] receiveData = new byte[512];
+    private byte[] sendData;
+    private byte[] receiveData = new byte[512];
     private long lastSendTime;
     private long lastReceiveTime;
-    private final int port = 4446;
+    private final int port;
     private UUID myUUID;
+    private UUID emptyUUID;
 
-    Client(String address) throws IOException {
+    Client(String address, int argport) throws IOException {
         multicastAddress = InetAddress.getByName(address);
+        port = argport;
         socket = new MulticastSocket(port);
         socket.joinGroup(multicastAddress);
-        //socket.setSoTimeout((int) timeout);
+        socket.setSoTimeout((int) timeout);
         aliveCopies = new HashMap<>();
-        lastReceiveTime = 4500;
+        lastReceiveTime = 0;
         lastSendTime = 0;
         myUUID = UUID.randomUUID();
+        emptyUUID = UUID.randomUUID();
         sendData = myUUID.toString().getBytes();
-        System.out.println("Your UUID: " + myUUID.toString());
+        //System.out.println("Your UUID: " + myUUID.toString());
     }
 
     public void findCopies() {
-        sendUDP();
-        //lastSendTime = System.currentTimeMillis();
         while (true) {
             if (System.currentTimeMillis() - lastSendTime > 1000) sendUDP();
             UUID receiveUUID = receiveUDP();
-            aliveCopies.put(receiveUUID, lastReceiveTime);
+            if (receiveUUID != emptyUUID)
+                aliveCopies.put(receiveUUID, lastReceiveTime);
+            aliveCopies.entrySet().removeIf(entry -> System.currentTimeMillis() - entry.getValue() > timeout);
             for (Map.Entry<UUID, Long> entry : aliveCopies.entrySet()) {
+                //if (System.currentTimeMillis() - entry.getValue() > timeout) clones.add(entry.getKey());
                 System.out.println("Node " + entry.getKey() + " is alive");
             }
-            aliveCopies.entrySet().removeIf(entry -> System.currentTimeMillis() - entry.getValue() > timeout);
-            }
         }
+    }
 
     private UUID receiveUDP() {
         DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
         try {
             socket.receive(receivePacket);
         } catch (IOException e) {
-            e.printStackTrace();
+            return emptyUUID;
         }
         lastReceiveTime = System.currentTimeMillis();
         return UUID.nameUUIDFromBytes(receiveData);
